861ddf185fd1c6d51d4ae46e86c372ddc5673920
ams557 <ams557-macos@As-MacBook-Pro.local>
2025-01-22 22:00:15 -0500

    Update ChessEngine.py
    Moving pieces - added move class

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,3 +1,4 @@
+
 class GameState():
     """
     This class is responsible for storing all the info about the current state of the game. It will also be responsible for
@@ -17,5 +18,53 @@ class GameState():
             ['--','--','--','--','--','--','--','--'],
             ['wp','wp','wp','wp','wp','wp','wp','wp'],
             ['wR','wN','wB','wQ','wK','wB','wN','wR']]
+
         self.whiteToMove=True
         self.moveLog=[]
+    
+    def makeMove(self,move):
+        """
+        Takes move as a parameter and executes it
+        """
+        self.board[move.endRow][move.endCol]=move.pieceMoved  # updates sq after move w/ the piece moved
+        self.board[move.startRow][move.startCol]='--'  # updates sq prior to move to empty
+        self.moveLog.append(move)  # log move sq to allow for undo
+        self.whiteToMove=not(self.whiteToMove)  # swap players
+
+class Move():
+    """
+    Chess notation involves rows classified by a number 1-8 while columns are classified by a letter a-f.
+    To use this notation, the [row][col] coordinates need to mapped to match the ones used in the original chess game.
+    """
+    # maps keys to values according to chess notation
+    # key = value
+    rankToRow = {'1':7,'2':6,'3':5,'4':4,
+                 '5':3,'6':2,'7':1,'8':0}
+    rowToRank = {v:k for k,v in rankToRow.items()}
+    fileToCol = {'a':0,'b':1,'c':2,'d':3,
+                 'e':4,'f':5,'g':6,'h':7}
+    colToFile = {v:k for k,v in fileToCol.items()}
+
+    def __init__(self,startSq, endSq, board):
+        self.startRow=startSq[0]
+        self.startCol=startSq[1]
+        self.endRow=endSq[0]
+        self.endCol=endSq[1]
+        self.pieceMoved=board[self.startRow][self.startCol]
+        self.pieceCaptured=board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        """
+        Converts input to pseudo chess notation.
+            - Note it is possible to make this real chess notation
+        """
+        return self.getRankFile(self.startRow,self.startCol)+self.getRankFile(self.endRow,self.endCol)
+
+    def getRankFile(self,row,col):
+        return self.colToFile[col]+self.rowToRank[row]
+
+
+
+
+
+        
