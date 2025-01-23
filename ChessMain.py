46a73d7305eb40a2d9d59ea5116531cc2772137d
ams557 <ams557-macos@As-MacBook-Pro.local>
2025-01-22 22:00:15 -0500

    Update ChessMain.py - Add movement

--- ChessMain.py
+++ ChessMain.py
@@ -31,26 +31,50 @@ def main():
     clock=p.time.Clock()
     screen.fill(p.Color('white'))
     gamestate=ChessEngine.GameState()
+ 
     loadImages()  #only done once before while loop
     running = True
-    while running: 
+    sqSelected = ()  # no sq is selected, keeps track of last click of user - tuple (row, col)
+    playerClicks = []  # keeps track of player clicks - two tuples: [(rows,cols),(rows up/down,cols left/right)]
+   
+    while running:
+        humanTurn=(gamestate.whiteToMove and playerOne) or (not(gamestate.whiteToMove) and playerTwo)  
         for event in p.event.get():
             if event.type==p.QUIT:
                 running=False
                 p.quit()
-                sys.exit()       
-               
+                sys.exit()
+            # mouse handler
+            elif event.type==p.MOUSEBUTTONDOWN:
+                    location=p.mouse.get_pos() # (x,y) location of mouse
+                    col=location[0]//SQ_SIZE
+                    row=location[1]//SQ_SIZE
+                    if sqSelected==(row,col) or col>=8:  # user hit same square twice
+                        sqSelected=()  # deselect
+                        playerClicks=[]  # clear player clicks
+                    else:
+                        sqSelected=(row,col)
+                        playerClicks.append(sqSelected)  # append for both 1st and 2nd clicks
+
+                    if len(playerClicks)==2:  # after 2nd click
+                        move = ChessEngine.Move(playerClicks[0],playerClicks[1],gamestate.board)
+                        print(move.getChessNotation())
+                        gamestate.makeMove(move)
+                        sqSelected=()  # reset user clicks
+                        playerClicks=[]  # clear player clicks
+        
+        drawGameState(screen,gamestate,validMoves,sqSelected,moveLogFont)    
         clock.tick(MAX_FPS)
         p.display.flip()
-        drawGameState(screen,gamestate)
 
 def drawGameState(screen, gamestate, validMoves, sqSelected,moveLogFont):
     """
     Function responsible for all graphics in current gamestate
     """
     drawBoard(screen)  # 1) draws squares on board
-    # 2) Highlight selectged squares (later)
+    # 2) Highlights selectged squares (Later)
     drawPieces(screen,gamestate.board)  # 3) draw pieces on top of squares
+    drawMoveLog(screen,gamestate,moveLogFont)
 
 def drawBoard(screen):
     """
